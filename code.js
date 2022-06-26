26544fd3afc93154068b250141bb2f0170ae3b2f
kLeister07 <kevinleister07@gmail.com>
2022-06-26 09:31:54 -0500

    first commit

---
+++ code.js
@@ -0,0 +1,6 @@
+var age = prompt("Enter your age")
+var yearsLeft= (90 - age);
+var monthsLeft= (yearsLeft * 12);
+var weeksLeft= (yearsLeft * 52);
+var daysLeft= (yearsLeft * 365);
+console.log("You have " + daysLeft + " days, " + weeksLeft + " weeks, and " + monthsLeft + " months left until 90 years old.");
